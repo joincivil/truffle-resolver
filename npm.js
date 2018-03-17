807a1caa33aa14c491312dc2f77dd9ec7c4a2f3a
Olaf Tomalka <olaf@tomalka.me>
2018-03-17 14:16:40 +0100

    Added Node's node_modules resolution algorithm

--- npm.js
+++ npm.js
@@ -1,45 +1,70 @@
 var path = require("path");
 var fs = require("fs");
+var doWhilst = require("async/doWhilst");
 
 function NPM(working_directory) {
   this.working_directory = working_directory;
 };
 
+// Needed to implement Node's module resolution algorith, as defined
+// here: https://nodejs.org/api/modules.html#modules_loading_from_node_modules_folders
+NPM.prototype.parent_directory = function(search_path) {
+  const root_path = path.parse(search_path).root;
+  if (search_path === root_path) {
+    return null;
+  }
+  const new_path = path.join(path.resolve(search_path, ".."))
+  return new_path;
+}
+
 NPM.prototype.require = function(import_path, search_path) {
+  // Don't try to load relative-path contracts
   if (import_path.indexOf(".") == 0 || import_path.indexOf("/") == 0) {
     return null;
   }
 
-  var separator = import_path.indexOf("/")
-  var package_name = import_path.substring(0, separator);
-  var contract_name = path.basename(import_path, ".sol");
+  const separator = import_path.indexOf("/")
+  const package_name = import_path.substring(0, separator);
+  const contract_name = path.basename(import_path, ".sol");
 
-  var expected_path = path.join((search_path || this.working_directory), "node_modules", package_name, "build", "contracts", contract_name + ".json");
-
-  try {
-    var result = fs.readFileSync(expected_path, "utf8");
-    return JSON.parse(result);
-  } catch (e) {
-    return null;
-  }
+  var current_dir = search_path || this.working_directory;
+  do {
+    const expected_path = path.join(current_dir, "node_modules", package_name, "build", "contracts", contract_name + ".json");
+    try {
+      const result = fs.readFileSync(expected_path, "utf8");
+      return JSON.parse(result);
+    } catch (e) {
+      // Empty on purpouse
+    }
+    current_dir = this.parent_directory(current_dir);
+  } while (current_dir !== null);
+  return null;
 };
 
 NPM.prototype.resolve = function(import_path, imported_from, callback) {
-  var expected_path = path.join(this.working_directory, "node_modules", import_path);
-
-  fs.readFile(expected_path, {encoding: "utf8"}, function(err, body) {
-    var resolved_path;
-    if (body) {
-      resolved_path = expected_path;
+  var current_directory = this.working_directory;
+  doWhilst(
+    function(next) {
+      const expected_path = path.join(current_directory, "node_modules", import_path);
+      current_directory = this.parent_directory(current_directory);
+      return fs.readFile(expected_path, {encoding: "utf8"}, function (err, body) {
+        // If there's an error, that means we can't read the source even if
+        // it exists. Treat it as if it doesn't by ignoring any errors.
+        // Perhaps we can do something better here in the future.
+        return next(null, body);
+      });
+    }.bind(this),
+    function(body) {
+      return current_directory !== null && !body;
+    },
+    function(err, body_arr) {
+      if (!body_arr) {
+        return callback(err, null, import_path);
+      }
+      // Note: resolved_path is the import path because these imports are special.
+      return callback(err, body_arr, import_path);
     }
-
-    // If there's an error, that means we can't read the source even if
-    // it exists. Treat it as if it doesn't by ignoring any errors.
-    // Perhaps we can do something better here in the future.
-
-    // Note: resolved_path is the import path because these imports are special.
-    return callback(null, body, import_path);
-  })
+  );
 };
 
 // We're resolving package paths to other package paths, not absolute paths.
